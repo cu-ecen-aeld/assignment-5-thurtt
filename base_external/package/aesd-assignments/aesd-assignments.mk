a88dc4cdfdd84e0884bfbb2940c1ab79e0e309a6
Thomas Hurtt <tom@mouse.local>
2026-08-09 22:33:46 -0400

    Adding the commit hashes for the assignment 9 driver changes

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '77e6f230241fc626fb16f3a63f229904472e7f3f'
+AESD_ASSIGNMENTS_VERSION = '79437c599111eb813bf14d384c5192af0c2ba504'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESDCHAR_VERSION = '1ceb287ca8cfdba584fec8d9379f06d567ac3720'
+AESDCHAR_VERSION = '79437c599111eb813bf14d384c5192af0c2ba504'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
